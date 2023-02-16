dc1959707fbf075596f99560cf1c3f1f3e6f1425
Random Game Dev <sebthproductivity@gmail.com>
2023-02-16 10:20:10 -0500

    Working on the MoveGameObject script

--- 2D platformer/Assets/_MyGameAssets/Environment/Scripts/MoveGameObject.cs
+++ 2D platformer/Assets/_MyGameAssets/Environment/Scripts/MoveGameObject.cs
@@ -1,35 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class MoveGameObject : MonoBehaviour
 {
-    [Header("Values: (only change speed and displacement)")]
-    [SerializeField] private float speed = 1.0f;
+    [Header("Values: ")]
+    [SerializeField] private TypeOfMovement m_typeOfMovement;
+    [SerializeField] private float m_timePerCycle;
+    [Header("Yoyo only: ")]
     [SerializeField] private Vector3 m_displacement;
     [SerializeField] private bool m_isSmooth;
+    [SerializeField] private float m_calculatedSpeed;
+    [Header("Rotation only: ")]
+    [SerializeField] private Transform m_rotationCenter;
+    [SerializeField] private float m_calculatedRadius;
+    [SerializeField] private float m_calculatedAngularVelocity;
+    [Header("Teleportation only: ")]
+    [SerializeField] private GameObject m_emptyPlatform;
+    [Header("Other: ")]
     [SerializeField] private Vector3 m_startPos;
     [SerializeField] private Vector3 m_endPos;
+
+    private enum TypeOfMovement
+    {
+        yoyo,
+        rotation,
+        teleportation
+    }
 
     private void Start()
     {
         m_startPos = transform.position;
-        m_endPos = m_startPos + m_displacement;
+
+        if (m_typeOfMovement == TypeOfMovement.yoyo)
+        {
+            m_endPos = m_startPos + m_displacement;
+            m_calculatedSpeed = m_displacement.magnitude / m_timePerCycle;
+        }
+
+        else if (m_typeOfMovement == TypeOfMovement.rotation)
+        {
+            m_calculatedAngularVelocity = 360.0f / m_timePerCycle;
+        }
+
+        else if (m_typeOfMovement == TypeOfMovement.teleportation)
+        {
+
+        }
     }
 
     void Update()
     {
-        //PingPong between 0 and 1
-        float time = Mathf.PingPong(Time.time * speed, 1);
+        if (m_typeOfMovement == TypeOfMovement.yoyo)
+        {
+            // PingPong between 0 and 1
+            float time = Mathf.PingPong(Time.time * (1/ m_timePerCycle), 1);
 
-        if (m_isSmooth)
-        {
-            transform.position = new Vector3(Mathf.SmoothStep(m_startPos.x, m_endPos.x, time),
-                Mathf.SmoothStep(m_startPos.y, m_endPos.y, time), 0);
+            if (m_isSmooth)
+            {
+                transform.position = new Vector3(Mathf.SmoothStep(m_startPos.x, m_endPos.x, time),
+                    Mathf.SmoothStep(m_startPos.y, m_endPos.y, time), 0);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(m_startPos, m_endPos, time);
+            }
         }
-        else
+
+        else if (m_typeOfMovement == TypeOfMovement.rotation)
         {
-            transform.position = Vector3.Lerp(m_startPos, m_endPos, time);
+            transform.RotateAround(m_rotationCenter.transform.position, Vector3.up, m_calculatedAngularVelocity * Time.deltaTime);
         }
     }
 }
