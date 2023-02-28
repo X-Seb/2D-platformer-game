8f3411e3add7cff19b6fd4d1536cbb8a6250d114
Random Game Dev <sebthproductivity@gmail.com>
2023-02-28 18:05:04 -0500

    Ready to work on dash practice

--- 2D platformer/Assets/_MyGameAssets/Environment/Scripts/ParralaxBackground.cs
+++ 2D platformer/Assets/_MyGameAssets/Environment/Scripts/ParralaxBackground.cs
@@ -30,23 +30,26 @@ public class ParralaxBackground : MonoBehaviour
 
     private void LateUpdate()
     {
-        // Figure out how much you've moved and move the object accordingly * the multipliers
-        m_deltaMovement = m_player.transform.position - m_lastPlayerPos;
-        m_deltaMovement.y *= m_yMultiplier;
-        m_deltaMovement.x *= m_xMultiplier;
-        transform.position += m_deltaMovement;
-        m_lastPlayerPos = m_player.transform.position;
-
-        if (m_infinityX && Mathf.Abs(m_player.transform.position.x - transform.position.x) >= m_textureSizeX)
+        if (GameManager.instance.GetState() != GameManager.GameState.lose)
         {
-            float offsetPosX = (m_player.transform.position.x - transform.position.x) % m_textureSizeX;
-            transform.position = new Vector3(m_player.transform.position.x + offsetPosX, transform.position.y, 0);
-        }
+            // Figure out how much you've moved and move the object accordingly * the multipliers
+            m_deltaMovement = m_player.transform.position - m_lastPlayerPos;
+            m_deltaMovement.y *= m_yMultiplier;
+            m_deltaMovement.x *= m_xMultiplier;
+            transform.position += m_deltaMovement;
+            m_lastPlayerPos = m_player.transform.position;
 
-        if (m_infinityY && Mathf.Abs(m_player.transform.position.y - transform.position.y) >= m_textureSizeY)
-        {
-            float offsetPosY = (m_player.transform.position.y - transform.position.y) % m_textureSizeY;
-            transform.position = new Vector3(transform.position.x, m_player.transform.position.y + offsetPosY, 0);
+            if (m_infinityX && Mathf.Abs(m_player.transform.position.x - transform.position.x) >= m_textureSizeX)
+            {
+                float offsetPosX = (m_player.transform.position.x - transform.position.x) % m_textureSizeX;
+                transform.position = new Vector3(m_player.transform.position.x + offsetPosX, transform.position.y, 0);
+            }
+
+            if (m_infinityY && Mathf.Abs(m_player.transform.position.y - transform.position.y) >= m_textureSizeY)
+            {
+                float offsetPosY = (m_player.transform.position.y - transform.position.y) % m_textureSizeY;
+                transform.position = new Vector3(transform.position.x, m_player.transform.position.y + offsetPosY, 0);
+            }
         }
     }
 }
