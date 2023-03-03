5c905a86c64f350e0ecda96c5b96130fe8c37e4c
Random Game Dev <sebthproductivity@gmail.com>
2023-03-03 14:39:09 -0500

    Started Green platform practice

--- 2D platformer/Assets/_MyGameAssets/Environment/Scripts/ObjectSpawner.cs
+++ 2D platformer/Assets/_MyGameAssets/Environment/Scripts/ObjectSpawner.cs
@@ -6,11 +6,10 @@ public class ObjectSpawner : MonoBehaviour
 {
     [Header("Important:")]
     [SerializeField] private float m_spawnInterval;
-    [SerializeField] private GameObject m_object;
-    [SerializeField] private bool m_spawnOnAwake;
-    [Tooltip("Only used for fireballs")]
-    [SerializeField] private Vector3 m_direction;
     [SerializeField] private float m_speed;
+    [SerializeField] private bool m_spawnOnAwake;
+    [SerializeField] private Vector3 m_direction;
+    [SerializeField] private Quaternion m_rotation;
     [Header("For reference only:")]
     [SerializeField] private bool m_isSpawning;
     [Header("Effects:")]
@@ -37,7 +36,7 @@ public class ObjectSpawner : MonoBehaviour
         {
             fireball.SetActive(true);
             fireball.gameObject.transform.position = m_spawnTrans.position;
-            fireball.gameObject.transform.rotation = m_spawnTrans.rotation;
+            fireball.gameObject.transform.rotation = m_rotation;
             fireball.GetComponent<FireballController>().Setup(m_direction, m_speed);
             m_audioSource.PlayOneShot(m_audioClip, m_volume);
             m_pfx.Play();
