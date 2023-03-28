98e8412a2ecb18087cd70029ff9e4ffa069fd37b
Random Game Dev <sebthproductivity@gmail.com>
2023-03-27 20:01:33 -0400

    Finished moving candle part
    I also extended the object spawner script.

--- 2D platformer/Assets/_MyGameAssets/Environment/Scripts/ObjectSpawner.cs
+++ 2D platformer/Assets/_MyGameAssets/Environment/Scripts/ObjectSpawner.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-    [Header("Important:")]
+    [Header("Spawning: ")]
     [SerializeField] private float m_spawnInterval;
+    [SerializeField] private bool m_randomizeSpawnInterval;
+    [SerializeField] private float m_minSpawnTime;
+    [SerializeField] private float m_maxSpawnTime;
+    [Header("Object speed: ")]
     [SerializeField] private float m_speed;
+    [SerializeField] private bool m_randomizeSpeed;
+    [SerializeField] private float m_minSpeedTime;
+    [SerializeField] private float m_maxSpeedTime;
+    [Header("Other: ")]
     [SerializeField] private bool m_spawnOnAwake;
     [SerializeField] private Vector3 m_direction;
     [SerializeField] private Quaternion m_rotation;
@@ -34,7 +42,6 @@ public class ObjectSpawner : MonoBehaviour
 
     private void OnEnable()
     {
-        Debug.Log("OnEnabled called from: " + gameObject.name);
         if (m_spawnOnAwake && m_isReady)
         {
             m_isSpawning = true;
@@ -54,21 +61,36 @@ public class ObjectSpawner : MonoBehaviour
 
     private IEnumerator SpawnObject(float time)
     {
-        Debug.Log("Going to create a fireball");
         GameObject fireball = ObjectPooler.instance.GetObject();
 
         if (fireball != null)
         {
+
             fireball.SetActive(true);
             fireball.transform.SetPositionAndRotation(m_spawnTrans.position, m_rotation);
-            fireball.GetComponent<FireballController>().Setup(m_direction, m_speed);
+
+            if (m_randomizeSpeed)
+            {
+                float speed = Random.Range(m_minSpeedTime, m_maxSpeedTime);
+                fireball.GetComponent<FireballController>().Setup(m_direction, speed);
+            }
+            else
+            {
+                fireball.GetComponent<FireballController>().Setup(m_direction, m_speed);
+            }
+
             m_audioSource.PlayOneShot(m_audioClip, m_volume);
             m_pfx.Play();
         }
 
         yield return new WaitForSeconds(time);
-        
-        if (m_isSpawning)
+
+        if (m_randomizeSpawnInterval && m_isSpawning)
+        {
+            float newTime = Random.Range(m_minSpawnTime, m_maxSpawnTime);
+            StartCoroutine(SpawnObject(newTime));
+        }
+        else if (m_isSpawning)
         {
             StartCoroutine(SpawnObject(m_spawnInterval));
         }
