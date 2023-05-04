76f59f0ea0961ff5e31f13c25720fa6abf3da2ec
Random Game Dev <sebthproductivity@gmail.com>
2023-05-03 20:01:10 -0400

    Ready to build the final level

--- 2D platformer/Assets/_MyGameAssets/Environment/Scripts/GateController.cs
+++ 2D platformer/Assets/_MyGameAssets/Environment/Scripts/GateController.cs
@@ -1,14 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
+using DG.Tweening;
 
 public class GateController : MonoBehaviour
 {
     [Header("Info: ")]
     [SerializeField] private int m_gateID;
-    [SerializeField] private CollectibleItem[] m_keys;
+    [SerializeField] private CollectibleItem[] m_requiredObjects;
     [Header("Setup")]
     [SerializeField] private Collider2D m_physicalCollider;
     [SerializeField] private Collider2D m_triggerCollider;
@@ -49,27 +48,32 @@ public class GateController : MonoBehaviour
         // Only open the gate if you collected, but didn't use one of the keys in the array
         // TODO: fix this since it's currently using up all the keys you have
         bool keyUsed = false; ;
-        for (int i = 0; i < m_keys.Length; i++)
+        for (int i = 0; i < m_requiredObjects.Length; i++)
         {
-            if (!keyUsed && PlayerPrefs.HasKey("Key_" + m_keys[i].keyID + "_Collected") &&
-                (!PlayerPrefs.HasKey("Key_" + m_keys[i].keyID + "_Used") || PlayerPrefs.GetInt("Key_" + m_keys[i].keyID + "_Used") == 0))
+            if (!keyUsed && PlayerPrefs.HasKey("Key_" + m_requiredObjects[i].keyID + "_Collected") &&
+                (!PlayerPrefs.HasKey("Key_" + m_requiredObjects[i].keyID + "_Used") || PlayerPrefs.GetInt("Key_" + m_requiredObjects[i].keyID + "_Used") == 0))
+            {
+                PlayerPrefs.SetInt("Key_" + m_requiredObjects[i].keyID + "_Used", 1);
+                keyUsed = true;
+                Debug.Log("Key " + m_requiredObjects[i].keyID + " used!");
+                break;
+            }
+
+            if (keyUsed)
             {
                 StartCoroutine(Unlock());
-                PlayerPrefs.SetInt("Key_" + m_keys[i].keyID + "_Used", 1);
-                keyUsed = true;
-                Debug.Log("Key " + m_keys[i].keyID + " used!");
             }
         }
     }
 
     private IEnumerator Unlock()
     {
-        // Effects: 
+        // Effects:
         m_triggerCollider.enabled = false;
         m_physicalCollider.enabled = false;
         m_spriteRenderer.color = new Color(1, 1, 1, 0.2f);
         m_audioSource.PlayOneShot(m_openAudioClip, m_volume);
-        m_light.intensity = 2.0f;
+        DOTween.To(() => m_light.intensity, x => m_light.intensity = x, 0.1f, 0.5f);
         //m_pfx.Play();
 
         PlayerPrefs.SetInt("Gate_" + m_gateID + "_Unlocked", 1);
